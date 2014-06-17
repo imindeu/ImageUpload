d949a84f49898b3bdf2643b60e2545febb372a88
László Kustra <laszlo.kustra@imind.eu>
2014-06-17 15:31:30 +0200

    Initial commit with the Android project configured.

---
+++ README.md
@@ -0,0 +1,5 @@
+ImageUpload
+===========
+
+Sample Android application that captures a camera image and uploads it to an API endpoint.
+
---
+++ android/ImageUpload/src/main/java/eu/imind/android/imageupload/view/HistoryActivity.java
@@ -0,0 +1,15 @@
+package eu.imind.android.imageupload.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import eu.imind.android.imageupload.R;
+
+public class HistoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
